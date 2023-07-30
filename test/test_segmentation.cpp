4497600964acf69107c671684ee55ba4788dc877
Acly <aclysia@gmail.com>
2023-07-30 18:52:34 +0200

    Removed include

--- test/test_segmentation.cpp
+++ test/test_segmentation.cpp
@@ -1,11 +1,11 @@
 #include "image.hpp"
 #include "segmentation.hpp"
 #include "test_utils.hpp"
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 #include <dlimgedit/dlimgedit.hpp>
 
-#include <format>
 #include <numeric>
 
 namespace dlimg {
